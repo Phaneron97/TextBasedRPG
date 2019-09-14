83393b54e41a50d379e397a42c7d4e73fb2720a4
Christian <christian.w@hotmail.nl>
2019-09-14 23:03:49 +0200

    shortenend printing player position, moving is now in loop

--- Main.py
+++ Main.py
@@ -4,17 +4,31 @@ from Classes import Room
 player = Player(1, 1)
 room1 = Room(5, 5)
 
-print("enter a direction to go in")
-direction = input("> ")
+def PlayerMove(x, y):
+
 
 i = 1
 for x in room1.door_list:
     print("door " + str(i) + " = [" + str(x.cell_x) + ", " + str(x.cell_y) + "]")
     i = i + 1
 
-# TODO: Move statements in loop
-if direction == "d":
-    player.current_cell_x += 1
-    print("current x: " + str(player.current_cell_x))
-    print("current y: " + str(player.current_cell_y))
+print("enter a direction to go in")
+
+moving = True
+while moving:
+    direction = input("> ")
+    if direction == "w":
+        player.current_cell_y += 1
+        print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+    if direction == "a":
+        player.current_cell_x -= 1
+        print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+    if direction == "s":
+        player.current_cell_y -= 1
+        print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+    if direction == "d":
+        player.current_cell_x += 1
+        print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+    if direction == "quit":
+        moving = False  # exit loop
 
