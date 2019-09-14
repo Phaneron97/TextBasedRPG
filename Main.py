460e6f8ef4c8103d068de57f0293e98f63b06efa
Christian <christian.w@hotmail.nl>
2019-09-14 21:37:55 +0200

    shortened door positions print, added todo

--- Main.py
+++ Main.py
@@ -7,10 +7,12 @@ room1 = Room(5, 5)
 print("enter a direction to go in")
 direction = input("> ")
 
+i = 1
 for x in room1.door_list:
-    print("door " + str(x) + "posX: " + str(x.cell_x))
-    print("door " + str(x) + "poxY: " + str(x.cell_y))
+    print("door " + str(i) + " = [" + str(x.cell_x) + ", " + str(x.cell_y) + "]")
+    i = i + 1
 
+# TODO: Move statements in loop
 if direction == "d":
     player.current_cell_x += 1
     print("current x: " + str(player.current_cell_x))
