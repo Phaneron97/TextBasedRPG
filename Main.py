f48516a15c82ee66d897822a885856d89359af24
Christian <christian.w@hotmail.nl>
2019-09-15 11:49:06 +0200

    Temp methods in file to shorten moving statements, added comments

--- Main.py
+++ Main.py
@@ -1,57 +1,64 @@
-from Classes import Player
-from Classes import Room
+from Classes import *
 
+
+# TODO: run methods from Methods.py
+# Methods here
+def PrintPlayerPos():
+    print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+
+
+def PrintWallError():
+    print("You reached the wall of the room you're in")
+
+
+# Objects here
 player = Player(1, 1)
 room1 = Room(5, 5)
 
-
+# Print doors in room
 i = 1
 for x in room1.door_list:
     print("door " + str(i) + " = [" + str(x.cell_x) + ", " + str(x.cell_y) + "]")
     i = i + 1
 
-print("enter a direction to go in")
+# print("enter a direction to go in")
 
 moving = True
 while moving:
 
     direction = input("> ")
-    # TODO: Move code within if statements to method
     if direction == "w":
         player.current_cell_y += 1
-        if player.current_cell_y <= room1.size_y:
-            print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+        if player.current_cell_y <= room1.size_y:  # if players Y pos is smaller then the room Y
+            PrintPlayerPos()  # Successfully moved in new direction
         else:
-            player.current_cell_y -= 1
-            print("You reached the wall of the room you're in")
+            player.current_cell_y -= 1  # Revert move to new direction to old position
+            PrintWallError()
 
     if direction == "a":
         player.current_cell_x -= 1
         if player.current_cell_x >= 1:
-            print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+            PrintPlayerPos()
         else:
             player.current_cell_x += 1
-            print("You reached the wall of the room you're in")
+            PrintWallError()
 
     if direction == "s":
         player.current_cell_y -= 1
         if player.current_cell_y >= 1:
-            print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+            PrintPlayerPos()
         else:
             player.current_cell_y += 1
-            print("You reached the wall of the room you're in")
+            PrintWallError()
 
     if direction == "d":
         player.current_cell_x += 1
         if player.current_cell_x <= room1.size_x:
-            print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+            PrintPlayerPos()
         else:
             player.current_cell_x -= 1
-            print("You reached the wall of the room you're in")
+            PrintWallError()
 
     if direction == "quit":
         moving = False  # exit loop
-
-print("User stopped moving")
-
-
+        print("User stopped moving")
