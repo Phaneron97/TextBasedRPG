2e7bb944d324d622fb7309208deceed6ee3594ae
Christian <christian.w@hotmail.nl>
2019-09-15 02:15:22 +0200

    Player can move in 2d world within room, boundaries of room are checked with each move

--- Main.py
+++ Main.py
@@ -4,8 +4,6 @@ from Classes import Room
 player = Player(1, 1)
 room1 = Room(5, 5)
 
-def PlayerMove(x, y):
-
 
 i = 1
 for x in room1.door_list:
@@ -16,19 +14,44 @@ print("enter a direction to go in")
 
 moving = True
 while moving:
+
     direction = input("> ")
+
     if direction == "w":
         player.current_cell_y += 1
-        print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+        if player.current_cell_y <= room1.size_y:
+            print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+        else:
+            player.current_cell_y -= 1
+            print("You reached the wall of the room you're in")
+
     if direction == "a":
         player.current_cell_x -= 1
-        print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+        if player.current_cell_x >= 1:
+            print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+        else:
+            player.current_cell_x += 1
+            print("You reached the wall of the room you're in")
+
     if direction == "s":
         player.current_cell_y -= 1
-        print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+        if player.current_cell_y >= 1:
+            print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+        else:
+            player.current_cell_y += 1
+            print("You reached the wall of the room you're in")
+
     if direction == "d":
         player.current_cell_x += 1
-        print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+        if player.current_cell_x <= room1.size_x:
+            print("player = [" + str(player.current_cell_x) + ", " + str(player.current_cell_y) + "]")
+        else:
+            player.current_cell_x -= 1
+            print("You reached the wall of the room you're in")
+
     if direction == "quit":
         moving = False  # exit loop
+
+print("Code ends here")
+
 
